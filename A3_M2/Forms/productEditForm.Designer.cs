b0131675d5c853f5a9de5769ab25f2d60c4d9b0e
omar-ahsan <106596821+omar-ahsan@users.noreply.github.com>
2023-11-29 21:40:26 +0500

    Insertion, Updation Forms Added. Insertion Logic 95% Done Review Insertion further, Updating Logic Remaining (#8)

---
+++ A3_M2/Forms/productEditForm.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace A3_M2.Forms
+{
+    partial class productEditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.insPolicy = new System.Windows.Forms.TextBox();
+            this.insExp = new System.Windows.Forms.Label();
+            this.insQuantity = new System.Windows.Forms.Label();
+            this.insDescription = new System.Windows.Forms.Label();
+            this.policyLabel = new System.Windows.Forms.Label();
+            this.batchLabel = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.insBatch = new System.Windows.Forms.TextBox();
+            this.insPrice = new System.Windows.Forms.TextBox();
+            this.insName = new System.Windows.Forms.TextBox();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.CloseButton);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.insPolicy);
+            this.panel1.Controls.Add(this.insExp);
+            this.panel1.Controls.Add(this.insQuantity);
+            this.panel1.Controls.Add(this.insDescription);
+            this.panel1.Controls.Add(this.policyLabel);
+            this.panel1.Controls.Add(this.batchLabel);
+            this.panel1.Controls.Add(this.priceLabel);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.insBatch);
+            this.panel1.Controls.Add(this.insPrice);
+            this.panel1.Controls.Add(this.insName);
+            this.panel1.Controls.Add(this.EditButton);
+            this.panel1.ForeColor = System.Drawing.Color.Lime;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(560, 349);
+            this.panel1.TabIndex = 0;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.CloseButton.ForeColor = System.Drawing.Color.Lime;
+            this.CloseButton.Location = new System.Drawing.Point(298, 281);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(89, 49);
+            this.CloseButton.TabIndex = 16;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = false;
+            this.CloseButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(179, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 23);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Product Updation Form";
+            // 
+            // textBox6
+            // 
+            this.textBox6.AcceptsReturn = true;
+            this.textBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox6.ForeColor = System.Drawing.Color.Lime;
+            this.textBox6.Location = new System.Drawing.Point(421, 153);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(112, 23);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.AcceptsReturn = true;
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox5.ForeColor = System.Drawing.Color.Lime;
+            this.textBox5.Location = new System.Drawing.Point(421, 105);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(112, 23);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.AcceptsReturn = true;
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox4.ForeColor = System.Drawing.Color.Lime;
+            this.textBox4.Location = new System.Drawing.Point(421, 54);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(112, 23);
+            this.textBox4.TabIndex = 12;
+            // 
+            // insPolicy
+            // 
+            this.insPolicy.AcceptsReturn = true;
+            this.insPolicy.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.insPolicy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.insPolicy.ForeColor = System.Drawing.Color.Lime;
+            this.insPolicy.Location = new System.Drawing.Point(156, 203);
+            this.insPolicy.Name = "insPolicy";
+            this.insPolicy.Size = new System.Drawing.Size(112, 23);
+            this.insPolicy.TabIndex = 11;
+            // 
+            // insExp
+            // 
+            this.insExp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.insExp.AutoSize = true;
+            this.insExp.ForeColor = System.Drawing.Color.White;
+            this.insExp.Location = new System.Drawing.Point(307, 156);
+            this.insExp.Name = "insExp";
+            this.insExp.Size = new System.Drawing.Size(66, 17);
+            this.insExp.TabIndex = 10;
+            this.insExp.Text = "Exp Date";
+            // 
+            // insQuantity
+            // 
+            this.insQuantity.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.insQuantity.AutoSize = true;
+            this.insQuantity.ForeColor = System.Drawing.Color.White;
+            this.insQuantity.Location = new System.Drawing.Point(307, 108);
+            this.insQuantity.Name = "insQuantity";
+            this.insQuantity.Size = new System.Drawing.Size(63, 17);
+            this.insQuantity.TabIndex = 9;
+            this.insQuantity.Text = "Quantity";
+            // 
+            // insDescription
+            // 
+            this.insDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.insDescription.AutoSize = true;
+            this.insDescription.ForeColor = System.Drawing.Color.White;
+            this.insDescription.Location = new System.Drawing.Point(307, 60);
+            this.insDescription.Name = "insDescription";
+            this.insDescription.Size = new System.Drawing.Size(80, 17);
+            this.insDescription.TabIndex = 8;
+            this.insDescription.Text = "Description";
+            // 
+            // policyLabel
+            // 
+            this.policyLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.policyLabel.AutoSize = true;
+            this.policyLabel.ForeColor = System.Drawing.Color.White;
+            this.policyLabel.Location = new System.Drawing.Point(30, 206);
+            this.policyLabel.Name = "policyLabel";
+            this.policyLabel.Size = new System.Drawing.Size(45, 17);
+            this.policyLabel.TabIndex = 7;
+            this.policyLabel.Text = "Policy";
+            // 
+            // batchLabel
+            // 
+            this.batchLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.batchLabel.AutoSize = true;
+            this.batchLabel.ForeColor = System.Drawing.Color.White;
+            this.batchLabel.Location = new System.Drawing.Point(30, 159);
+            this.batchLabel.Name = "batchLabel";
+            this.batchLabel.Size = new System.Drawing.Size(101, 17);
+            this.batchLabel.TabIndex = 6;
+            this.batchLabel.Text = "Batch Number";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.ForeColor = System.Drawing.Color.White;
+            this.priceLabel.Location = new System.Drawing.Point(30, 111);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.priceLabel.Size = new System.Drawing.Size(39, 17);
+            this.priceLabel.TabIndex = 5;
+            this.priceLabel.Text = "Price";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(28, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Product Name";
+            // 
+            // insBatch
+            // 
+            this.insBatch.AcceptsReturn = true;
+            this.insBatch.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.insBatch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.insBatch.ForeColor = System.Drawing.Color.Lime;
+            this.insBatch.Location = new System.Drawing.Point(156, 153);
+            this.insBatch.Name = "insBatch";
+            this.insBatch.Size = new System.Drawing.Size(112, 23);
+            this.insBatch.TabIndex = 3;
+            // 
+            // insPrice
+            // 
+            this.insPrice.AcceptsReturn = true;
+            this.insPrice.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.insPrice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.insPrice.ForeColor = System.Drawing.Color.Lime;
+            this.insPrice.Location = new System.Drawing.Point(156, 105);
+            this.insPrice.Name = "insPrice";
+            this.insPrice.Size = new System.Drawing.Size(112, 23);
+            this.insPrice.TabIndex = 2;
+            // 
+            // insName
+            // 
+            this.insName.AcceptsReturn = true;
+            this.insName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.insName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.insName.ForeColor = System.Drawing.Color.Lime;
+            this.insName.Location = new System.Drawing.Point(156, 54);
+            this.insName.Name = "insName";
+            this.insName.Size = new System.Drawing.Size(112, 23);
+            this.insName.TabIndex = 1;
+            // 
+            // EditButton
+            // 
+            this.EditButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.EditButton.ForeColor = System.Drawing.Color.Lime;
+            this.EditButton.Location = new System.Drawing.Point(183, 281);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(89, 49);
+            this.EditButton.TabIndex = 0;
+            this.EditButton.Text = "Edit";
+            this.EditButton.UseVisualStyleBackColor = false;
+            this.EditButton.Click += new System.EventHandler(this.InsertButton_Click);
+            // 
+            // productEditForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(584, 373);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "productEditForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox insName;
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox insBatch;
+        private System.Windows.Forms.TextBox insPrice;
+        private System.Windows.Forms.Label insQuantity;
+        private System.Windows.Forms.Label insDescription;
+        private System.Windows.Forms.Label policyLabel;
+        private System.Windows.Forms.Label batchLabel;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox insPolicy;
+        private System.Windows.Forms.Label insExp;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
